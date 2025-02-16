1e66fbed7d1aef69b542bfa717e3ac501dd4b1a1
prop07 <rijalmanoj07@gmail.com>
2025-02-16 15:10:04 +0545

    bug fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,51 +58,43 @@ const Home = () => {
           <h1 className=" text-center font-bold pb-8 px-2 text-4xl tracking-normal">
             Help You Find Space?
           </h1>
-          <div className=" flex justify-center">
-            <div
-              style={{ width: `${width - 18}px` }}
-              className=" max-w-[600px] "
-            >
-              <input
-                onChange={(e) => setSearchQuery(e.target.value.trim())}
-                onKeyDown={handleKeyDown}
-                className="bg-secondary pb-2 pt-3 px-3 w-full rounded-t-md focus:outline-none border-t border-x border-outlineWhite"
-                type="text"
-                placeholder="Enter space code !"
-                id="inputField"
-                autoFocus
-              />
-              <label htmlFor="inputField">
-                <div className="bg-secondary cursor-text flex justify-between  rounded-b-md px-3 py-3 border-b border-x border-outlineWhite">
-                  <button
-                    onClick={generateSpace}
-                    className="bg-secondary text-sm flex items-center gap-2  font-semibold text-neutral-300 cursor-pointer hover:bg-neutral-700 border py-1 px-2 rounded-md border-outlineWhite "
-                  >
-                    Create Space <FaPlus className="text-white" size={13} />
-                  </button>
-                  <span className="h-10 w-10 flex justify-center items-center">
-                    {searchQuery && status === "pending" ? (
-                      <CgSpinner
-                        className="animate-spin text-white"
-                        size={21}
-                      />
-                    ) : searchQuery && status === "success" ? (
-                      <Link to={`space?id=${data.space_code}`}>
-                        <button className="text-sm font-semibold border py-2 px-2 rounded-md bg-neutral-100 border-neutral-100 text-black cursor-pointer">
-                          <FaArrowUp className="rotate-90" />
-                        </button>
-                      </Link>
-                    ) : (
-                      <Toolip title={"Enter valid code"}>
-                        <button className="text-sm font-semibold border py-2 px-2 rounded-md bg-neutral-700 border-neutral-700 text-secondary cursor-default">
-                          <FaArrowUp className="rotate-90" />
-                        </button>
-                      </Toolip>
-                    )}
-                  </span>
-                </div>
-              </label>
-            </div>
+          <div className=" w-[90vw] max-w-lg mx-auto rounded-xl bg-secondary">
+            <input
+              onChange={(e) => setSearchQuery(e.target.value.trim())}
+              onKeyDown={handleKeyDown}
+              className=" bg-transparent pb-2 pt-3 px-3 w-full  focus:outline-none"
+              type="text"
+              placeholder="Enter space code !"
+              id="inputField"
+              autoFocus
+            />
+            <label htmlFor="inputField">
+              <div className=" cursor-text flex justify-between   px-3 py-3">
+                <button
+                  onClick={generateSpace}
+                  className=" text-sm flex items-center gap-2  font-semibold text-white cursor-pointer hover:bg-neutral-600 border py-1 px-2 rounded-md border-neutral-400 hover:border-neutral-600 active:bg-neutral-700 active:border-neutral-700"
+                >
+                  Create Space <FaPlus className="text-white" size={13} />
+                </button>
+                <span className="h-10 w-10 flex justify-center items-center">
+                  {searchQuery && status === "pending" ? (
+                    <CgSpinner className="animate-spin text-white" size={21} />
+                  ) : searchQuery && status === "success" ? (
+                    <Link to={`space?id=${data.space_code}`}>
+                      <button className="text-sm font-semibold border py-2 px-2 rounded-md bg-neutral-100 border-neutral-100 text-black cursor-pointer">
+                        <FaArrowUp className="rotate-90" />
+                      </button>
+                    </Link>
+                  ) : (
+                    <Toolip title={"Enter valid code"}>
+                      <button className="text-sm font-semibold border py-2 px-2 rounded-md bg-neutral-700 border-neutral-700 text-secondary cursor-default">
+                        <FaArrowUp className="rotate-90" />
+                      </button>
+                    </Toolip>
+                  )}
+                </span>
+              </div>
+            </label>
           </div>
           <div className="flex justify-center">
             <button
